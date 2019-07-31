ea5d1d23fab7473a13fb9da31370805b762080cd
José Gilberto Serrano González <50712307+Gilberto12345@users.noreply.github.com>
2019-07-30 22:18:43 -0500

    dia3

--- express-examples/FirstWebPage/src/index.js
+++ express-examples/FirstWebPage/src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
 // settings
 app.set('port', 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // express se entiende con ejs
 
 // middlewares
+
+// routes
+app.use(require('./routes/index'));
 
 // static fies
 
---
+++ express-examples/FirstWebPage/src/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) => {
+    
+    res.render('index', {title: 'First Website'});
+
+});
+
+router.get('/contact', (req,res) => {
+    
+    res.render('contact', {title: 'Contact Page'});
+
+});
+
+module.exports = router;
